1b0ab0303dd55df3a74ec7d8c5b6c161f314e1cf
Hieu Ho <hihho@ucdavis.edu>
2020-03-30 20:37:31 -0700

    Removed colors, moved functionality to stylesheet

--- src/spinDancer.js
+++ src/spinDancer.js
@@ -1,6 +1,5 @@
 var SpinDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.colorWheel = ['yellow', 'green', 'blue'];
   this.$node.addClass('spin');
 };
 
@@ -8,10 +7,5 @@ SpinDancer.prototype = Object.create(Dancer.prototype);
 SpinDancer.prototype.constructor = SpinDancer;
 
 SpinDancer.prototype.step = function() {
-  // let self = this;
   Dancer.prototype.step.call(this);
-  let color1 = Math.floor(Math.random() * 256);
-  let color2 = Math.floor(Math.random() * 256);
-  let color3 = Math.floor(Math.random() * 256);
-  this.$node.css('background-color', 'rgb("+ color1 + ","+ color2 +", "+ color3 +")');
 };
