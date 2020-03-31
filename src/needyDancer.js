ecc9849229c1b7b192fb9e78c96928d68152093b
Hieu Ho <hihho@ucdavis.edu>
2020-03-31 16:16:26 -0700

    Changed starting position, add changeColor

--- src/needyDancer.js
+++ src/needyDancer.js
@@ -1,29 +1,29 @@
 var NeedyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('needy');
-  this.top = 50%;
-  this.left = 50%;
+  this.top = 500;
+  this.left = 500;
 };
 
 NeedyDancer.prototype = Object.create(Dancer.prototype);
 NeedyDancer.prototype.constructor = NeedyDancer;
 
 NeedyDancer.prototype.step = function() {
-  this.attach();
   Dancer.prototype.step.call(this);
+  this.changeColor();
+  this.attach();
 };
 
 NeedyDancer.prototype.attach = function() {
   let n = window.dancers.length;
-  console.log(n)
   let leftTotal = 0;
   let topTotal = 0;
   for (var i = 0; i < window.dancers.length; i++) {
     let currentDancer = window.dancers[i];
     leftTotal += currentDancer.left;
-    topTotal += currentDancer.top
+    topTotal += currentDancer.top;
   }
   this.left = leftTotal / n;
-  this.top = topTotal /  n;
+  this.top = topTotal / n;
   this.setPosition(this.top, this.left);
 };
