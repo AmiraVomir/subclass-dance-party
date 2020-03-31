6a2eacc3b7b65107d849865b62e932d7e4c3de2e
Hieu Ho <hihho@ucdavis.edu>
2020-03-31 12:14:26 -0700

    Added chase methods

--- src/needyDancer.js
+++ src/needyDancer.js
@@ -1,15 +1,25 @@
 var NeedyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node.addClass('spin');
+  this.$node.addClass('needy');
 };
 
 NeedyDancer.prototype = Object.create(Dancer.prototype);
 NeedyDancer.prototype.constructor = NeedyDancer;
 
 NeedyDancer.prototype.step = function() {
+  this.attach();
   Dancer.prototype.step.call(this);
 };
 
 NeedyDancer.prototype.attach = function() {
-
+  let n = window.dancers.length;
+  let leftAvg = 0;
+  let topAvg = 0;
+  for (const dancer of window.dancers) {
+    leftAvg += dancer.left;
+    topAvg += dancer.top;
+  }
+  this.left = leftAvg / n;
+  this.top = topAvg / n;
+  this.setPosition(this.left, this.top);
 };
