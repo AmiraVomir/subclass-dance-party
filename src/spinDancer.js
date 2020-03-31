c9a3a3f3296bf24aeca0c0ea0f0d32c3b6d06581
Hieu Ho <hihho@ucdavis.edu>
2020-03-30 19:18:23 -0700

    Attempted to change color

--- src/spinDancer.js
+++ src/spinDancer.js
@@ -1,11 +1,17 @@
 var SpinDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.colorWheel = ['yellow', 'green', 'blue'];
+  this.$node.addClass('spin');
 };
 
 SpinDancer.prototype = Object.create(Dancer.prototype);
 SpinDancer.prototype.constructor = SpinDancer;
 
-SpinDancer.prototype.spin = function() {
+SpinDancer.prototype.step = function() {
+  // let self = this;
   Dancer.prototype.step.call(this);
-  this.$node.addClass('spin');
+  let color1 = Math.floor(Math.random() * 256);
+  let color2 = Math.floor(Math.random() * 256);
+  let color3 = Math.floor(Math.random() * 256);
+  this.$node.css('background-color', 'rgb("+ color1 + ","+ color2 +", "+ color3 +")');
 };
