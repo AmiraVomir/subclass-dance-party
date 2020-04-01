6aa93445070d4ec8a3d4b6fc9292b56dc9edd9ba
Hieu Ho <hihho@ucdavis.edu>
2020-03-31 17:54:48 -0700

    Added color change function

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -16,4 +16,5 @@ BlinkyDancer.prototype.step = function() {
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
+  this.changeColor();
 };
