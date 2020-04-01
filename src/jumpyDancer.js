8a2b599ec347fe06d67c755ee6ea96bd632b4c33
Hieu Ho <hihho@ucdavis.edu>
2020-03-31 17:55:40 -0700

    Added color change function

--- src/jumpyDancer.js
+++ src/jumpyDancer.js
@@ -3,15 +3,10 @@ var JumpyDancer = function(top, left, timeBetweenSteps) {
   this.$node.addClass('jumpy');
 };
 
-
 JumpyDancer.prototype = Object.create(Dancer.prototype);
 JumpyDancer.prototype.constructor = JumpyDancer;
 
 JumpyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
-};
-
-JumpyDancer.prototype.lineUp = function(value) {
-  this.$node.stop();
-  this.setPosition(value, 25);
+  this.changeColor();
 };
